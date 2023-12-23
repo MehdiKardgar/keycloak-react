57f76a55c0145329a5a23a73eb9a1b04ce5b0b97
Mehdi Kardgar <76148286+MehdiKardgar@users.noreply.github.com>
2023-12-23 20:26:42 +0330

    put token in the localStorage

--- src/components/Protected.js
+++ src/components/Protected.js
@@ -3,7 +3,21 @@ import KeycloakClient from "../KeycloakClient";
 
 const Protected = ({ token }) => {
   // here you can get the token and use it in your http calls
-  console.log(token);
+  // console.log(token);
+  console.log(KeycloakClient.tokenParsed);
+
+  if (KeycloakClient && KeycloakClient.tokenParsed) {
+    const username = KeycloakClient.tokenParsed.preferred_username; // Accessing username
+    const name = KeycloakClient.tokenParsed.name; // Accessing name
+
+    console.log("Username:", username);
+    console.log("Name:", name);
+
+    localStorage.setItem("username", username);
+    localStorage.setItem("name", name);
+  } else {
+    console.log("Token not parsed or Keycloak instance not available");
+  }
 
   return (
     <div>
